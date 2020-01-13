15367960138ebdbd289295aca711409a5d5959d4
FreddieDev <freddiechessell83@gmail.com>
2020-01-13 13:03:01 +0000

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using System.Xml;
+
+namespace WhosTurn
+{
+    public class Game
+    {
+        // Settings
+        private int CountdownDuration = 2;
+
+        // Vars
+        private DispatcherTimer dispatcherTimer;
+        public List<int> KeysInGame { get; protected set; }
+        private int TimeUntilStart;
+        private bool GameOver;
+
+        public Game()
+        {
+            KeysInGame = new List<int>();
+        }
+
+        private void UpdateCountdownText()
+        {
+            MainWindow.SetCountdownText("Game starting in " + TimeUntilStart + " seconds...");
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            TimeUntilStart--;
+
+            if (TimeUntilStart < 1)
+            {
+                GameOver = true; // Update flag to stop leaving players clearing results screen
+
+                var random = new Random();
+                int winnerIndex = random.Next(KeysInGame.Count);
+                MainWindow.DisplayWinner(KeysInGame[winnerIndex]);
+
+                dispatcherTimer.Stop();
+            } else
+            {
+                UpdateCountdownText();
+            }
+        }
+
+        public void StartCountdown()
+        {
+            StopCountdown();
+            TimeUntilStart = CountdownDuration;
+            UpdateCountdownText();
+
+            //  DispatcherTimer setup
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void StopCountdown()
+        {
+            if (dispatcherTimer != null)
+            {
+                dispatcherTimer.Stop();
+            }
+        }
+
+        public void UpdateGame()
+        {
+            // (re)start game countdown if more than 1 person is playing
+            bool shouldStartGame = KeysInGame.Count > 1;
+            if (shouldStartGame)
+            {
+                StartCountdown();
+            }
+            else
+            {
+                StopCountdown();
+            }
+            MainWindow.SetCountdownVisibility(shouldStartGame);
+        }
+
+        public void AddKey(int asciiCode)
+        {
+            if (!KeysInGame.Contains(asciiCode))
+            {
+                KeysInGame.Add(asciiCode);
+                GameOver = false;
+                UpdateGame();
+            }
+        }
+
+        public void RemoveKey(int asciiCode)
+        {
+            KeysInGame.Remove(asciiCode);
+            if (!GameOver) UpdateGame();
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WhosTurn
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Static form. Null if no form created yet.
+        private static MainWindow form = null;
+        private Game whosTurn;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            whosTurn = new Game();
+            form = this;
+        }
+
+        private static string GetKeyLetterFromNumber(int letterNumber)
+        {
+            KeysConverter kc = new KeysConverter();
+            return kc.ConvertToString(letterNumber);
+        }
+
+        public static void SetCountdownText(String countdownString)
+        {
+            form.L_Countdown.Content = countdownString;
+        }
+
+        internal static void DisplayWinner(int winningKey)
+        {
+            form.L_Countdown.Content = "";
+            form.L_Status.Content = "It's " + GetKeyLetterFromNumber(winningKey) + "'s turn!";
+        }
+
+        public static void SetCountdownVisibility(bool countdownVisibility)
+        {
+            form.L_Countdown.Visibility = countdownVisibility ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void UpdateKeysDisplay()
+        {
+            String keysText = "Held keys: ";
+            foreach (int i in whosTurn.KeysInGame)
+            {
+                keysText += i + " ";
+            }
+
+            //L_Status.Content = keysText;
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            int asciiCode = KeyInterop.VirtualKeyFromKey(e.Key);
+            whosTurn.AddKey(asciiCode);
+
+            UpdateKeysDisplay();
+        }
+
+        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            int asciiCode = KeyInterop.VirtualKeyFromKey(e.Key);
+            whosTurn.RemoveKey(asciiCode);
+
+            UpdateKeysDisplay();
+        }
+    }
+}
